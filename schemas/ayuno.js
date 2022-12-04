8f3c8d2d9a6f85f762b41ba4aa01b2f32ecb4875
Michael Ng <kngz@ucenfotec.ac.cr>
2022-12-03 19:43:38 -0600

    Se arreglaron las rutas del calendario

--- schemas/ayuno.js
+++ schemas/ayuno.js
@@ -2,11 +2,11 @@ var mongoose = require('mongoose');
 
 var ayunoSchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
-    correo: String,
     horas_ayuno: String,
     hora_comienzo: String,
     hora_fin: String,
-    fecha: String
+    fecha: String,
+    idUsuario: String
 });
 
 //Permite que node pueda leer el archivo
